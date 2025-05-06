5768bba6a141a1d8d88597a51306591dac1c8757
tb-peregrine <cameronarcher@tinybird.co>
2025-05-06 11:34:52 -0500

    Changed color definitions for better visual distinction between good/better/best models

--- src/src/app/components/progress.tsx
+++ src/src/app/components/progress.tsx
@@ -1,8 +1,10 @@
 export const ProgressBar = ({ progress }: { progress: number }) => {
   const color =
-    progress > 50
+    progress >= 90
       ? "bg-[#27F795]"
-      : "bg-[#FF6153]";
+      : progress >= 80
+        ? "bg-[#F7D727]"
+        : "bg-[#F72727]";
 
   return (
     <div className="w-16 bg-[#353535] h-2 mr-2">
