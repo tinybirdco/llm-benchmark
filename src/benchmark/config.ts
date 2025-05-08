eb9506d6349eb2c66500c1230b53d46de9188c64
Luis Rodríguez <luis@tinybird.co>
2025-05-08 12:44:33 +0200

    Make env vars consistent with documentation

--- src/benchmark/config.ts
+++ src/benchmark/config.ts
@@ -11,11 +11,11 @@ export function getConfig() {
       explorationId: process.env.EXPLORATION_ID!,
       explorationName: process.env.EXPLORATION_NAME!,
       
-      apiHost: process.env.API_HOST!,
+      apiHost: process.env.TINYBIRD_API_HOST!,
       chatApiEndpoint: process.env.CHAT_API_ENDPOINT!,
 
-      workspaceId: process.env.WORKSPACE_ID!,
-      workspaceToken: process.env.WORKSPACE_TOKEN!,
+      workspaceId: process.env.TINYBIRD_WORKSPACE_ID!,
+      workspaceToken: process.env.TINYBIRD_WORKSPACE_TOKEN!,
       
     },
   };
